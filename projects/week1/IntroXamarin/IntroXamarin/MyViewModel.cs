b221ce8ea32c67b9901305bed24b477fafe6f141
Kevin Ford <4380198+Bowman74@users.noreply.github.com>
2021-02-25 15:04:05 -0800

    First week presentation

---
+++ projects/week1/IntroXamarin/IntroXamarin/MyViewModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.ComponentModel;
+
+namespace IntroXamarin
+{
+    public class MyViewModel: INotifyPropertyChanged
+    {
+        public MyViewModel()
+        {
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private string _myProperty = "starting value";
+
+        public string MyProperty
+        {
+            get { return _myProperty;  }
+            set
+            {
+                _myProperty = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(MyProperty)));
+            }
+        }
+    }
+}
